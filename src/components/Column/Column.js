5f1cc5007beaf544b020ace12a93ba555c361509
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-06-13 11:20:04 +0200

    create Card.js and Icon.js in components

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import {settings, listData} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator';
+import Card from '../Card/Card.js';
+import Icon from '../Icon/Icon.js';
+
 
 class Column extends React.Component {
-  static propTypes = {
-    title: PropTypes.string,
+  state = {
+    cards: this.props.cards || [],
   }
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}>{this.props.title} 
+          <span className={styles.icon}>
+            <Icon name={this.props.icon} />
+          </span>
+        </h3>
+        <div>
+          {this.state.cards.map(({key, title}) => (
+          <Card key={key} title={title}/>
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+        </div>
       </section>
     )
+  }
+  
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+          }
+        ]
+      }
+    ));
   }
 }
 
