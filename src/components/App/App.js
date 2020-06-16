ea7f4a720f5613e86592088a512a425a78a75235
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-06-16 20:19:20 +0200

    Add ESLint and husky

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
-import {pageContents, listData} from '../../data/dataStore';
+import {pageContents, listData , settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator';
 
 class App extends React.Component {
   render() {
@@ -9,10 +10,25 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <List {...listData}/>
+        <List {...listData} />
+        <div className={styles.creator}>
+          <Creator text={settings.listCreatorText} action={title => this.addList(title)} />
+        </div>
       </main>
-    )
+    );
+  }
+  
+  addList(){
+    this.setState(state => (
+      {
+        list: [
+          ...state.list,
+        ],
+      }
+    ));
   }
 }
+
+
 
 export default App;
