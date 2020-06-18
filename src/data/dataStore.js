9ec9bf75fa627a6ad1e448defa4066a295e4c4dc
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-06-18 19:39:47 +0200

    rewrite

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -12,7 +12,7 @@ export const settings = {
 };
 
 export const pageContents = {
-  title: 'My first React app',
+  title: 'Mi first React app',
   subtitle: 'A simple to-do app, with lists, columns and cards',
 };
 
